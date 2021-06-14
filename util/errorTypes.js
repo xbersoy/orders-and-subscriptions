a98b1491cde88c3610e4694694350bcbc77d4619
xbersoy <burakersoy@msn.com>
2021-06-14 03:14:12 +0300

    error type generator

---
+++ util/errorTypes.js
@@ -0,0 +1,24 @@
+class GeneralError extends Error {
+    constructor(message) {
+      super();
+      this.message = message;
+    }
+  
+    getStatusCode() {
+      if (this instanceof BadRequest) return 400;
+      if (this instanceof NotFound) return 404;
+      if (this instanceof InternalServerError) return 500;
+      return 500;
+    }
+  }
+  
+  class BadRequest extends GeneralError {}
+  class NotFound extends GeneralError {}
+  class InternalServerError extends GeneralError {}
+  
+  module.exports = {
+    GeneralError,
+    BadRequest,
+    NotFound,
+    InternalServerError
+  };
